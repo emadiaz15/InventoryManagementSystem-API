7a9c04aae537a563142eb184c6e7c935a9ecdf30
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2024-11-13 23:15:11 -0300

    "Added create_category view and updated category_list view to separate GET and POST requests; refactored category_detail view to handle GET, PUT, and DELETE requests"

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from apps.products.api.views.category import category_list, category_detail
+from apps.products.api.views.category import category_list, category_detail, create_category  # Importamos la nueva vista `create_category`
 from apps.products.api.views.types import type_list, type_detail, create_type
 from apps.products.api.views.products import product_list, product_detail
 
 urlpatterns = [
     # Rutas de Categorías
-    path('categories/', category_list, name='category-list'),
-    path('categories/<int:pk>/', category_detail, name='category-detail'),
+    path('categories/', category_list, name='category-list'),  # Ruta para listar categorías activas
+    path('categories/create/', create_category, name='category-create'),  # Nueva ruta para crear una categoría
+    path('categories/<int:pk>/', category_detail, name='category-detail'),  # Ruta para detalles de una categoría específica
 
- # Rutas de Tipos
+    # Rutas de Tipos
     path('types/', type_list, name='type-list'),  # Ruta para listar tipos activos
     path('types/create/', create_type, name='type-create'),  # Nueva ruta para crear un tipo
     path('types/<int:pk>/', type_detail, name='type-detail'),  # Ruta para detalles de un tipo específico
 
     # Rutas de Productos
-    path('products/', product_list, name='product-list'),
-    path('products/<int:pk>/', product_detail, name='product-detail'),  # Corrección en el nombre de la ruta a plural
+    path('products/', product_list, name='product-list'),  # Ruta para listar productos
+    path('products/<int:pk>/', product_detail, name='product-detail'),  # Ruta para detalles de un producto específico
 ]
 
 # Añadir soporte para sufijos de formatos (e.g., /products.json)
--- apps/products/api/views/category.py
+++ apps/products/api/views/category.py
@@ -1,3 +1,5 @@
+# Este archivo define los endpoints para listar, crear, obtener, actualizar y eliminar (de manera suave) categorías de productos en la API.
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -6,62 +8,75 @@ from apps.products.models import Category
 from ..serializers import CategorySerializer
 from drf_spectacular.utils import extend_schema
 
+# Vista para listar todas las categorías activas
 @extend_schema(
     methods=['GET'],
     operation_id="list_categories",
-    description="Retrieve a list of all categories",
+    description="Recupera una lista de todas las categorías",
     responses={200: CategorySerializer(many=True)},
 )
-@extend_schema(
-    methods=['POST'],
-    operation_id="create_category",
-    description="Create a new category",
-    request=CategorySerializer,
-    responses={
-        201: CategorySerializer,
-        400: "Bad Request - Invalid data",
-    },
-)
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def category_list(request):
     """
-    Endpoint para obtener una lista de categorías o crear una nueva categoría.
+    Endpoint para listar solo las categorías activas.
     """
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    elif request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            # Asociar la categoría con el usuario autenticado, si es necesario
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Recupera todas las categorías activas de la base de datos
+    categories = Category.objects.filter(is_active=True)
+    # Serializa los datos para enviarlos en la respuesta
+    serializer = CategorySerializer(categories, many=True)
+    # Devuelve los datos serializados con un código de estado HTTP 200 OK
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
+# Vista para crear una nueva categoría
+@extend_schema(
+    methods=['POST'],
+    operation_id="create_category",
+    description="Crea una nueva categoría",
+    request=CategorySerializer,
+    responses={
+        201: CategorySerializer,
+        400: "Solicitud Incorrecta - Datos inválidos",
+    },
+)
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_category(request):
+    """
+    Endpoint para crear una nueva categoría.
+    """
+    # Serializa los datos recibidos en la solicitud
+    serializer = CategorySerializer(data=request.data)
+    # Valida los datos y, si son válidos, guarda la nueva categoría asociada al usuario autenticado
+    if serializer.is_valid():
+        serializer.save(user=request.user)  # Asocia la categoría con el usuario autenticado
+        # Responde con los datos de la nueva categoría y un código de estado HTTP 201 CREATED
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # Si los datos no son válidos, responde con los errores de validación y un código HTTP 400 BAD REQUEST
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Vista para obtener, actualizar o eliminar una categoría específica
 @extend_schema(
     methods=['GET'],
     operation_id="retrieve_category",
-    description="Retrieve details of a specific category",
-    responses={200: CategorySerializer, 404: "Category not found"},
+    description="Recupera detalles de una categoría específica",
+    responses={200: CategorySerializer, 404: "Categoría no encontrada"},
 )
 @extend_schema(
     methods=['PUT'],
     operation_id="update_category",
-    description="Update details of a specific category",
+    description="Actualiza detalles de una categoría específica",
     request=CategorySerializer,
     responses={
         200: CategorySerializer,
-        400: "Bad Request - Invalid data",
+        400: "Solicitud Incorrecta - Datos inválidos",
     },
 )
 @extend_schema(
     methods=['DELETE'],
     operation_id="delete_category",
-    description="Delete a specific category",
-    responses={204: "Category deleted successfully", 404: "Category not found"},
+    description="Marca una categoría específica como inactiva",
+    responses={204: "Categoría eliminada (soft) correctamente", 404: "Categoría no encontrada"},
 )
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
@@ -69,22 +84,31 @@ def category_detail(request, pk):
     """
     Endpoint para obtener, actualizar o eliminar una categoría específica.
     """
+    # Intenta obtener la categoría por su clave primaria (pk); si no existe, responde con un error 404
     try:
         category = Category.objects.get(pk=pk)
     except Category.DoesNotExist:
         return Response({"detail": "Categoría no encontrada."}, status=status.HTTP_404_NOT_FOUND)
     
+    # Maneja la solicitud GET para obtener detalles de la categoría
     if request.method == 'GET':
         serializer = CategorySerializer(category)
         return Response(serializer.data)
     
+    # Maneja la solicitud PUT para actualizar los detalles de la categoría
     elif request.method == 'PUT':
         serializer = CategorySerializer(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        # Responde con errores de validación si los datos no son válidos
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    # Maneja la solicitud DELETE para marcar la categoría como inactiva
     elif request.method == 'DELETE':
         category.delete()
-        return Response({"detail": "Categoría eliminada correctamente."}, status=status.HTTP_204_NO_CONTENT)
+        # Devuelve una respuesta de éxito indicando que la categoría ha sido eliminada (soft)
+        return Response(
+            {"detail": "Categoría eliminada (soft) correctamente."},
+            status=status.HTTP_204_NO_CONTENT
+        )
