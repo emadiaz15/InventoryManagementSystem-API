0d368a6a3df3aa13db275fa02cee77d357bae2bb
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-02-26 01:13:18 -0300

    `Added perfil/ to .gitignore and added static media URL handling to inventory_management/urls.py`

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Rutas de la API
 api_patterns = [
     path('users/', include('apps.users.api.urls')),  # Rutas de la app `users`
     path('inventory/', include('apps.products.api.urls')),  # Rutas de la app `products`
     path('cutting/', include('apps.cuts.api.urls')),  # Rutas de la app `cutting`
-    path('', include('apps.core.urls')),  # Rutas de la app `core` (Añadido)
+    path('', include('apps.core.urls')),  # Rutas de la app `core`
 ]
 
 # Rutas para la documentación de la API con drf-spectacular
@@ -18,9 +20,11 @@ schema_patterns = [
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # Documentación en ReDoc
 ]
 
-# URL principal
 urlpatterns = [
     path('admin/', admin.site.urls),  # Ruta al panel de administración de Django
     path('api/v1/', include(api_patterns)),  # Incluye todas las rutas de la API organizadas
     path('api-docs/', include(schema_patterns)),  # Incluye las rutas para la documentación de la API
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
