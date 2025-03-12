8736c3109d7f8346a61d0959bb7ab766b30b8868
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-03-12 11:26:35 -0300

    `Update filename in LOGGING configuration in production settings`

--- inventory_management/settings/production.py
+++ inventory_management/settings/production.py
@@ -47,7 +47,7 @@ LOGGING = {
         'file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
-            'filename': './app/logs/django-error.log',  # Asegúrate de que esta ruta exista
+            'filename': './logs/django-error.log',  # Asegúrate de que esta ruta exista
         },
     },
     'loggers': {
