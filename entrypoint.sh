31ccedbf307fdd8c1b5754f7f9a4d900f9559713
Emanuel Diaz <108106363+emadiaz15@users.noreply.github.com>
2025-04-11 15:04:50 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 # Salir inmediatamente si un comando falla
 set -e
 
-# Ejecutar las migraciones de Django
-# --noinput evita que pida confirmaciones interactivas
-echo "Aplicando migraciones de base de datos..."
+echo "▶️ Realizando makemigrations..."
+python manage.py makemigrations --noinput
+
+echo "▶️ Aplicando migraciones de base de datos..."
 python manage.py migrate --noinput
 
 # Ejecutar el comando principal que se pasó al contenedor
-# (En tu caso, será: python manage.py runserver 0.0.0.0:8000)
 exec "$@"
