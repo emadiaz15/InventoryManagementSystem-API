3cd8ac3d99dc44afb2c3feb7d033466541565692
Emanuel Diaz <108106363+emadiaz15@users.noreply.github.com>
2025-04-11 15:16:24 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "🔧 Aplicando migraciones de base de datos..."
-python manage.py makemigrations --noinput  # 👉 Asegúrate de incluir esto
+python manage.py makemigrations stocks users products cuts --noinput  # 👉 Asegúrate de incluir esto
 python manage.py migrate --noinput
 
 echo "🚀 Iniciando servidor Django..."
