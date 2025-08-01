89ad9a46dc13355b1743eda35fd4864d0b432697
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2025-08-01 00:13:36 -0300

    Add validations for unique product code, valid category, and type

--- apps/products/filters/type_filter.py
+++ apps/products/filters/type_filter.py
@@ -2,23 +2,19 @@ import django_filters
 from apps.products.models.type_model import Type 
 
 class TypeFilter(django_filters.FilterSet):
-    """
-    Filtro para el modelo Type.
-    Permite filtrar por:
-      - el nombre del tipo (campo "name") de forma parcial, case-insensitive.
-      - el nombre de la categoría asociada (campo "category__name") de forma parcial, case-insensitive.
-    """
     name = django_filters.CharFilter(
         field_name='name',
-        lookup_expr='icontains',  # Contiene, sin importar mayúsculas/minúsculas
-        label='Filtrar por nombre del tipo (contiene)'
+        lookup_expr='icontains'
     )
     category = django_filters.CharFilter(
         field_name='category__name',
-        lookup_expr='icontains',  # Filtra por nombre de la categoría asociada
-        label='Filtrar por nombre de la categoría (contiene)'
+        lookup_expr='icontains'
+    )
+    category_id = django_filters.NumberFilter(
+        field_name='category_id',
+        label='Filtrar por ID de la categoría'
     )
 
     class Meta:
         model = Type
-        fields = ['name', 'category']
+        fields = ['name', 'category', 'category_id']
